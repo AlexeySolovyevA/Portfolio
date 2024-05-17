5afb5fdeafa4cef9dae7d5a81f5717c79e17f9cf
Aleksey <mrlehasolovey@ya.ru>
2024-05-17 20:48:13 +0300

    feat: I wrote a function to close the menu when clicking on the link

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 const burger = document.querySelector('#burger');
-const menu = document.querySelector('#menu')
+const menu = document.querySelector('#menu');
+const anchorLinks = document.querySelectorAll('.anchor-link');
+
 burger.addEventListener('click' , () => {
     menu.classList.toggle('open');
-})
+});
+
+// Переписать код на Event родителя
+anchorLinks.forEach(link => {
+    link.addEventListener('click' , () => {
+        menu.classList.remove('open');
+    })
+});
